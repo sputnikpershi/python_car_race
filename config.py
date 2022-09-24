10a3f52a8d22b24b490016e2f630f78697bd5410
Lenis Merino <45776323+lenismerino@users.noreply.github.com>
2022-09-24 19:05:04 +0800

    Updated configuration values

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 This module has the default configuration values for the project
 """
 
-number_of_consumer_cars: int = 2
-number_of_sport_cars: int = 3
-max_distance: int = 1496
-speed_decay_range: tuple = (5, 12)
-speed_increase_range: tuple = (15, 35)
-speed_range_consumer_car: tuple = (250, 750)
-speed_range_sport_car: tuple = (600, 2000)
+number_of_consumer_cars: int = 8
+number_of_sport_cars: int = 5
+max_distance: int = 5000
+speed_decay_range: tuple = (3, 30)
+speed_increase_range: tuple = (15, 50)
+speed_range_consumer_car: tuple = (250, 900)
+speed_range_sport_car: tuple = (400, 2500)
